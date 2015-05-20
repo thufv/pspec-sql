1d9ca4f9b0bacd091f3081605ce668e8f5ca755f
luochen <luochen01@vip.qq.com>
2015-05-20 11:32:45 +0800

    remove experiment project

--- experiment/src/edu/thu/ss/experiment/query/Test.scala
+++
@@ -1,8 +0,0 @@
-package edu.thu.ss.experiment.query
-
-object Test extends App {
-  val runtime = Runtime.getRuntime();
-  val process = runtime.exec("java", Array("edu.thu.ss.experiment.query"));
-  process.waitFor();
-  println(process.exitValue());
-}
